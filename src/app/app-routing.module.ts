cfd880e6d28f673c77f12a1435a2e9c3d8096192
Gautam mishra <gmishra@ciena.com>
2020-09-12 01:55:49 +0530

    add hashocationstrategy for routing error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ component: ProjectComponent
 }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
